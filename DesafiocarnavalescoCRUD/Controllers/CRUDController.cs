24698be59024d7d94f68ba8c245b35bf0d5cb739
Fernando Mota <fmsilva@id.uff.br>
2018-02-06 21:33:05 -0200

    initial commit

---
+++ DesafiocarnavalescoCRUD/Controllers/CRUDController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesafiocarnavalescoCRUD.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/CRUD")]
+    public class CRUDController : Controller
+    {
+	    [HttpGet, Route("read")]
+	    public async Task<IActionResult> Read()
+	    {
+		    try
+		    {
+			    string[] lines = System.IO.File.ReadAllLines(@"C:\Users\fmota\Desktop\teste\testando.txt");
+			    return Accepted(lines);
+		    }
+		    catch (Exception e)
+		    {
+			    return new StatusCodeResult(500);
+		    }
+	    }
+    }
+}
